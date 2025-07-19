2af937da8468d8bf966d6579a0b70ebe5795286c
Hager706 <hagertarek706@gmail.com>
2025-07-19 12:03:49 +0300

    2 commit

---
+++ LAB1/index.js
@@ -0,0 +1,12 @@
+// Simple JavaScript to print a message
+console.log("Hello from JavaScript!");
+
+// Do a simple math operation
+let a = 5;
+let b = 3;
+let sum = a + b;
+
+console.log("The sum of a and b is:", sum);
+
+// Display an alert (works in browsers)
+alert("This is a simple JavaScript alert!");
